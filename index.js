e9e247f630d8d48491ef151866cc548e43e6c54e
Payal Yadav <pty22112000@gmail.com>
2025-01-03 07:13:03 -0800

    some small changes

--- index.js
+++ index.js
@@ -3,20 +3,27 @@ import 'dotenv/config';
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Home Page!');
+// app.get('/', (req, res) => {
+//   res.send('Home Page!');
+// });
+
+// get a list of 5 jokes
+
+app.get('/jokes', (req, res) => {
+  const jokes = [
+    { id: 1, setup: "Why did the developer go broke?", punchline: "Because he used up all his cache." },
+    { id: 2, setup: "Why do programmers prefer dark mode?", punchline: "Because light attracts bugs!" },
+    { id: 3, setup: "How do you comfort a JavaScript bug?", punchline: "You console it." },
+    { id: 4, setup: "Why was the JavaScript developer sad?", punchline: "Because he didn’t know how to 'null' his feelings." },
+    { id: 5, setup: "Why don’t programmers like nature?", punchline: "It has too many bugs." }
+  ];
+  res.send(jokes);
 });
 
-app.get('/login', (req, res) => {
-  res.send('Login Page!');
-});
-
-app.get('/register', (req, res) => {
-  res.send('Register Page!');
-});
+// end a 5 list of jokes
 
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
-  console.log(`Development server listening on port ${port}`);
+  console.log(`Development server listening on http://localhost:${port}`);
 });
