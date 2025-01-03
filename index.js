b9865673200ac6b80d514cde41a4ca562a06c813
Payal Yadav <pty22112000@gmail.com>
2025-01-03 06:39:22 -0800

    routing completed

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
 import express from 'express';
+import 'dotenv/config';
 
-const app = express()
-
-const port = 5000;
+const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Home Page!')
+  res.send('Home Page!');
 });
 
 app.get('/login', (req, res) => {
-  res.send('Login Page!')
+  res.send('Login Page!');
 });
 
 app.get('/register', (req, res) => {
-  res.send('Register Page!')
+  res.send('Register Page!');
 });
+
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log(`Development server listening on port ${port}`);
